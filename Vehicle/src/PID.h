8bdf30cf612e053345334057791781f199e9a05a
LiaoCheng <LiaoChengO@qq.com>
2024-12-20 13:21:46 +0800

    PID: add delta time

--- Vehicle/src/PID.h
+++ Vehicle/src/PID.h
@@ -3,15 +3,21 @@
 class PID
 {
 public:
+    PID(float deltaTime = 1) :
+        dt(deltaTime)
+    {}
     // 1. pars need to be set
     // 1.1 manually
-    float Kp, Ki, Kd;
+    float Kp = 0;
+    float Ki = 0;
+    float Kd = 0;
     float output_min = FLT_MIN;
     float output_max = FLT_MAX;
     float integral_limit = FLT_MAX;
     // 1.2 automatically by program
-    float target;
-    float actual;
+    float target = 0;
+    float actual = 0;
+    const int dt;  // int may be better than float
 
     // just for remember output
     float output;
@@ -42,7 +48,7 @@ public:
         else if (integral < -integral_limit)
             integral = -integral_limit;
 
-        output = Kp * error + Ki * integral + Kd * (error - error_last);
+        output = Kp * error + Ki * integral * dt + Kd * (error - error_last) / dt;
         if (output > output_max) output = output_max;
         else if (output < output_min) output = output_min;
 
@@ -53,7 +59,7 @@ public:
     float CalOutput_Inc() {
         error = target - actual;
 
-        float output_dt = Kp * (error - error_last) + Ki * error + Kd * (error - 2*error_last + error_last_last);
+        float output_dt = Kp * (error - error_last) + Ki * error * dt + Kd * (error - 2*error_last + error_last_last) / dt;
         output += output_dt;
         if (output > output_max) output = output_max;
         else if (output < output_min) output = output_min;
